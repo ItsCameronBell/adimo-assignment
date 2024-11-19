e028a7da5aced2148bc2fd161637ad24a2b35769
ItsCameronBell <cameron352bell@gmail.com>
2024-11-19 00:49:57 +0000

    feat: collected products details

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
 import axios from 'axios';
 import { JSDOM } from 'jsdom';
 
+const imagePath = 'https://cdn.adimo.co/clients/Adimo/test/';
+
 axios
   .get<string>('https://cdn.adimo.co/clients/Adimo/test/index.html')
   .then((response) => {
-    // HTML is inside response.data
+    if (response.status != 200) {
+      throw new Error('Unsuccessful website response');
+    }
     const dom: JSDOM = new JSDOM(response.data);
-    console.log(dom);
-    console.log('Hello, World!');
+    const products: HTMLCollection =
+      dom.window.document.getElementsByClassName('item');
+
+    for (const product of products) {
+      const heading = product.querySelector('h1');
+      if (heading) {
+        const title: string = heading.innerHTML;
+        console.log(title);
+      }
+      const img = product.querySelector('img');
+      if (img) {
+        const imgURL: string = img.src;
+        console.log(imagePath.concat(imgURL));
+      }
+      const priceWithCurrency = product.querySelector('.price');
+      if (priceWithCurrency) {
+        const price: number = +priceWithCurrency.innerHTML.replace('£', '');
+        console.log(price);
+      }
+    }
   })
   .catch((error: unknown) => {
     console.log(error);
