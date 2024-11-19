2264bf8719d7412baa89e48693c1eb68d832c3e6
ItsCameronBell <cameron352bell@gmail.com>
2024-11-19 15:20:33 +0000

    feat: calculated total number of items, average price and compiled that to a json

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+export interface Product {
+  title?: string;
+  imgURL?: string;
+  price?: number;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { JSDOM } from 'jsdom';
 
+import * as fs from 'fs';
+
+import { Product } from './index.d';
+
 const imagePath = 'https://cdn.adimo.co/clients/Adimo/test/';
 
 axios
@@ -10,26 +14,53 @@ axios
       throw new Error('Unsuccessful website response');
     }
     const dom: JSDOM = new JSDOM(response.data);
-    const products: HTMLCollection =
+    const items: HTMLCollection =
       dom.window.document.getElementsByClassName('item');
 
-    for (const product of products) {
-      const heading = product.querySelector('h1');
+    const json = [];
+
+    let averagePrice = 0;
+
+    const products: Product[] = [];
+    for (const item of items) {
+      const product: Product = {};
+      const heading = item.querySelector('h1');
       if (heading) {
         const title: string = heading.innerHTML;
-        console.log(title);
+        product.title = title;
       }
-      const img = product.querySelector('img');
+      const img = item.querySelector('img');
       if (img) {
         const imgURL: string = img.src;
-        console.log(imagePath.concat(imgURL));
+        product.imgURL = imagePath.concat(imgURL);
       }
-      const priceWithCurrency = product.querySelector('.price');
+      const priceWithCurrency = item.querySelector('.price');
       if (priceWithCurrency) {
         const price: number = +priceWithCurrency.innerHTML.replace('£', '');
-        console.log(price);
+        product.price = price;
+        averagePrice += price;
       }
+      products.push(product);
     }
+
+    json.push({
+      noOfProducts: items.length,
+      averagePrice: averagePrice / items.length,
+    });
+    json.push(products);
+
+    fs.writeFile(
+      './data/results.json',
+      JSON.stringify(json),
+      'utf8',
+      (error: unknown) => {
+        if (error) {
+          console.error('Error writing to file', error);
+        } else {
+          console.log('Results written to file');
+        }
+      }
+    );
   })
   .catch((error: unknown) => {
     console.log(error);
